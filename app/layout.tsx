abd28a06b3843060cb18add8876685d5397b89e0
LIUDAXINGTX <liudaxingtx@gmail.com>
2024-02-16 22:52:45 -0600

    chapter 1 to 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
+import '@/app/ui/global.css';
+import {primaryText} from "@/app/ui/fonts";
+
+const RootLayout = ({children}: { children: React.ReactNode; }) => (
     <html lang="en">
-      <body>{children}</body>
+        <body className={`${primaryText.className} antialiased`}>
+            {children}
+        </body>
     </html>
-  );
-}
+);
+
+export default RootLayout;
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter, Lusitana} from 'next/font/google';
+
+export const primaryText = Inter({subsets: ['latin']});
+
+export const secondaryText = Lusitana({weight: ['400','700'], subsets: ["latin"]});
