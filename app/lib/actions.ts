58ac8fe6a00d19bdc5ca15b299172721fb148c86
LIUDAXINGTX <liudaxingtx@gmail.com>
2024-03-20 22:46:37 -0500

    Error handler

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,10 +12,12 @@ export const createInvoice = async (formData: FormData) => {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  console.log({ customerId, amountInCents, status, date });
-  await sql`INSERT INTO invoices (customer_id, amount, status, date)
-              VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
-
+  try {
+    await sql`INSERT INTO invoices (customer_id, amount, status, date)
+                  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Create Invoice.' };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 };
@@ -26,18 +28,29 @@ export const updateInvoice = async (id: string, formData: FormData) => {
   );
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-
-  await sql`UPDATE invoices
-            SET customer_id = ${customerId},
-                amount      = ${amountInCents},
-                status      = ${status},
-                date        = ${date}
-            WHERE id = ${id}`;
+  try {
+    await sql`UPDATE invoices
+                  SET customer_id = ${customerId},
+                      amount      = ${amountInCents},
+                      status      = ${status},
+                      date        = ${date}
+                  WHERE id = ${id}`;
+  } catch (error) {
+    return { message: 'Update Invoice Failed.' };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 };
 
 export const deleteInvoice = async (id: string) => {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE
+                  FROM invoices
+                  WHERE id = ${id}`;
+  } catch (error) {
+    return { message: 'Delete Invoice Failed...' };
+  }
+
   revalidatePath('/dashboard/invoices');
+  return { message: 'Invoice Deleted.' };
 };
